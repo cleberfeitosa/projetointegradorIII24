0d99bc4238f22870abbd57ad402ecf4bb4fd9a09
Cleber Feitosa <cleber.feitosa@ifmt.edu.br>
2024-12-04 10:52:47 -0400

    Project, Collections and Environment

---
+++ teste/src/main/java/com/tads/dscatalog/dto/UserUpdateDTO.java
@@ -0,0 +1,8 @@
+package com.tads.dscatalog.dto;
+
+import com.tads.dscatalog.services.validation.UserUpdateValid;
+
+@UserUpdateValid
+public class UserUpdateDTO extends UserDTO {
+
+}
---
+++ teste/src/main/java/com/tads/dscatalog/projections/UserDetailsProjection.java
@@ -0,0 +1,9 @@
+package com.tads.dscatalog.projections;
+
+public interface UserDetailsProjection {
+
+	String getUsername();
+	String getPassword();
+	Long getRoleId();
+	String getAuthority();
+}
